16a7611330296936beac364feb1333522246b829
Jainil Ranpara <jainilr.ap@gmail.com>
2025-10-17 19:52:36 +0530

    Sidebar Fix

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -27,7 +27,7 @@
                 <a data-bs-target="#dashboards" data-bs-toggle="collapse" class="sidebar-link">
                     <i class="align-middle me-2 fas fa-fw fa-users"></i> <span class="align-middle">Internal Users Management</span>
                 </a>
-                <ul id="dashboards" class="sidebar-dropdown list-unstyled collapse show" data-bs-parent="#sidebar">
+                <ul id="dashboards" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                     <li class="sidebar-item @if(request()->segment(1) == 'users') active @endif"><a class='sidebar-link' href='{{ route('users.index') }}'>Users</a></li>
                     <li class="sidebar-item @if(request()->segment(1) == 'roles') active @endif"><a class='sidebar-link' href='{{ route('roles.index') }}'>Roles</a></li>
                 </ul>
@@ -36,10 +36,10 @@
 
             @if(auth()->user()->isAdmin() || auth()->user()->can('customers.index') || auth()->user()->can('locations.index'))
             <li class="sidebar-item @if( request()->segment(1) == 'customers' || request()->segment(1) == 'locations') active @endif">
-                <a data-bs-target="#dashboards" data-bs-toggle="collapse" class="sidebar-link">
+                <a data-bs-target="#customerMgmt" data-bs-toggle="collapse" class="sidebar-link">
                     <i class="align-middle me-2 fas fa-fw fa-user-tie"></i> <span class="align-middle">Customers Management</span>
                 </a>
-                <ul id="dashboards" class="sidebar-dropdown list-unstyled collapse show" data-bs-parent="#sidebar">
+                <ul id="customerMgmt" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                     <li class="sidebar-item @if(request()->segment(1) == 'customers') active @endif"><a class='sidebar-link' href='{{ route('customers.index') }}'>Customers</a></li>
                     <li class="sidebar-item @if(request()->segment(1) == 'locations') active @endif"><a class='sidebar-link' href='{{ route('locations.index') }}'>Locations</a></li>
                 </ul>
@@ -59,7 +59,7 @@
                 <a data-bs-target="#catalogMenu" data-bs-toggle="collapse" class="sidebar-link">
                     <i class="align-middle me-2 fas fa-fw fa-boxes"></i> <span class="align-middle">Catalog</span>
                 </a>
-                <ul id="catalogMenu" class="sidebar-dropdown list-unstyled collapse show" data-bs-parent="#sidebar">
+                <ul id="catalogMenu" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
                     @if(auth()->user()->isAdmin() || auth()->user()->can('categories.index'))
                     <li class="sidebar-item @if(request()->segment(1) == 'categories') active @endif"><a class='sidebar-link' href='{{ route('categories.index') }}'>Categories</a></li>
                     @endif
